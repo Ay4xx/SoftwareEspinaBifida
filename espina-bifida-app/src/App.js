c286f96060ce67affe30e72b94eeb0899226825a
Fiorella de Medina <fiorella.demedinac@gmail.com>
2026-04-06 14:34:34 -0600

    nav y registro de citas
    nav y registro de citas check, se puede hacer el registro pero aun no se guarda

--- espina-bifida-app/src/App.js
+++ espina-bifida-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Sidebar from './componentes/sidebar/sidebar';
 import './App.css';
 import Header from './componentes/header/header';
+import Tabnav from './componentes/tabnav/tabnav';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import UsuariosPage from './pantallas/usuario';
 import HistorialPage from './pantallas/historial';
+import ServiciosPanel from './pantallas/inventario';
 //import Login from './pages/login';
 
 function App() {
@@ -22,6 +24,7 @@ function App() {
             <Routes>
               <Route path="/" element={<UsuariosPage />} />
               <Route path="/historial" element={<HistorialPage />} />
+              <Route path="/inventario" element={<ServiciosPanel />} />
             </Routes>
           </div>
 
