7b6221e62ea01636f55cb1c742a2440acddf620f
Fiorella de Medina <fiorella.demedinac@gmail.com>
2026-04-12 16:33:22 -0600

    arreglar font
    arreglar front con carta de detalle

--- espina-bifida-app/src/pantallas/inventario.jsx
+++ espina-bifida-app/src/pantallas/inventario.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import TabNav from '../componentes/tabnav/tabnav'; // 
+import TabNav from '../componentes/tabnav/tabnav';
 import RegistrarConsulta from '../componentes/registrocitas/registrocitas';
 import Medicamentos from '../componentes/medicamentos/medicamentos';
 import EquipoMedico from '../componentes/equipomedico/equipomedico';
+import VisualizarInfo from '../componentes/detallepaciente/detallepaciente';
 import { FileText, Pill, Users } from "lucide-react";
-
+import "./inventario.css";
 
 const tabs = [
   { id: "citas", label: "Citas", icon: <FileText size={16} /> },
@@ -12,18 +13,40 @@ const tabs = [
   { id: "equipo", label: "Equipo médico", icon: <Users size={16} /> },
 ];
 
+const pacientee = {
+  nombre: "María García López",
+  iniciales: "MG",
+  curp: "MAGL031599MNL",
+  email: "maria.garcia@email.com",
+  telefono: "81 1234 5678",
+  ubicacion: "Nuevo León",
+  registro: "2022-03-15",
+  estado: "Activo",
+  vencimiento: "30-03-2026"
+};
+
 function ServiciosPanel() {
   const [activeTab, setActiveTab] = useState("citas");
 
   return (
-    <div>
-      <TabNav tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
+    <div className="inventario-contenedor">
 
-      <div className="tab-content">
-        {activeTab === "citas" &&  <RegistrarConsulta />}
-        {activeTab === "medicamentos" && <Medicamentos />}
-        {activeTab === "equipo" && <EquipoMedico />}
+      {/* IZQUIERDA — Info del paciente */}
+      <div className="inventario-izq">
+        <VisualizarInfo paciente={pacientee} />
       </div>
+
+      {/* DERECHA — Tabs y contenido */}
+      <div className="inventario-derecho">
+        <TabNav tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
+
+        <div className="tab-content">
+          {activeTab === "citas" && <RegistrarConsulta />}
+          {activeTab === "medicamentos" && <Medicamentos />}
+          {activeTab === "equipo" && <EquipoMedico />}
+        </div>
+      </div>
+
     </div>
   );
 }
