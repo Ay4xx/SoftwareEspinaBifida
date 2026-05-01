86bd92546dedf580a420ad2a2a44641ea94c41e6
MarioBBT11 <mariobeauregard2004@gmail.com>
2026-04-30 19:35:10 -0600

    pruebas e2e qase
    Pruebas sobre notificaiones

---
+++ espina-bifida-app/playwright.config.js
@@ -0,0 +1,46 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html'],
+    [
+      'playwright-qase-reporter',
+      {
+        mode: 'testops',
+        debug: true,
+        testops: {
+          api: {
+            token: 'TU_TOKEN_AQUI',
+          },
+          project: 'HS01',
+          run: {
+            complete: true,
+          },
+        },
+      },
+    ],
+  ],
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
