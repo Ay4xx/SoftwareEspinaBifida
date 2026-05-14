69d0b68f3f25f603a8ea2a8a2d253984c1c93689
MarioBBT11 <mariobeauregard2004@gmail.com>
2026-05-14 17:44:28 -0600

    editar usario
    Se anadio la opcion de anadir una foto al usuario la ucal se actualiza en el momento

--- espina-bifida-backend/modulos/gestionUsuarios/gestionUsuarios.routes.js
+++ espina-bifida-backend/modulos/gestionUsuarios/gestionUsuarios.routes.js
@@ -1,37 +1,12 @@
-import { Router } from "express";
-import { listar, obtener, crear, actualizar, eliminar } from "./gestionUsuarios.controller.js";
+import express from "express";
+import { listar, obtener, crear, actualizar, eliminar, uploadFoto } from "./gestionUsuarios.controller.js";
 
-const router = Router();
+const router = express.Router();
 
-
-function autenticar(req, res, next) {
-    const token = req.headers["authorization"]?.split(" ")[1];
-
-    if (!token)
-        return res.status(401).json({ ok: false, message: "Token requerido" });
-
-
-    const match = token.match(/^token-(\d+)-([^-]+)-\d+$/);
-        if (!match)
-            return res.status(401).json({ ok: false, message: "Token inválido" });
-
-    req.usuario = { id: Number(match[1]), tipoUsuario: match[2] };
-    next();
-    }
-
-
-function soloAdmin(req, res, next) {
-  const tipo = req.usuario.tipoUsuario?.toUpperCase();
-  if (tipo !== "ADMINISTRADOR" && tipo !== "SUPERADMIN")
-    return res.status(403).json({ ok: false, message: "Solo los administradores pueden realizar esta acción" });
-  next();
-}
-
-
-router.get   ("/",    autenticar,            listar);
-router.get   ("/:id", autenticar,            obtener);
-router.post  ("/",    autenticar, soloAdmin, crear);
-router.put   ("/:id", autenticar, soloAdmin, actualizar);
-router.delete("/:id", autenticar, soloAdmin, eliminar);
+router.get("/",       listar);
+router.get("/:id",    obtener);
+router.post("/",      uploadFoto, crear);
+router.put("/:id",    uploadFoto, actualizar);
+router.delete("/:id", eliminar);
 
 export default router;
