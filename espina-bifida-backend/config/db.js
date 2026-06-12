88f9ae1ad1c23d851740822a0ab3c1ed62c94109
Fiorella de Medina <fiorella.demedinac@gmail.com>
2026-06-12 01:06:44 -0600

    ultimo 2

--- espina-bifida-backend/config/db.js
+++ espina-bifida-backend/config/db.js
@@ -1,21 +1,15 @@
-import oracledb from "oracledb";
-import "dotenv/config";
-
-// Configuración global necesaria para Thin mode
-oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-
 export async function getConnection() {
-  try {
-    return await oracledb.getConnection({
-      // Con el Wallet, el usuario y la contraseña están guardados dentro del archivo cwallet.sso
-      // Por lo tanto, no es estrictamente necesario pasarlos aquí si el wallet está bien configurado.
-      connectString: process.env.DB_CONNECT_STRING,
-      configDir: process.env.TNS_ADMIN,
-      walletLocation: process.env.TNS_ADMIN,
-      walletPassword: process.env.WALLET_PASSWORD
-    });
-  } catch (err) {
-    console.error("Error al conectar a la BD:", err);
-    throw err;
-  }
+  return await oracledb.getConnection({
+    // El usuario debe ir aquí, pero NO la contraseña
+    user: process.env.DB_USER, 
+    
+    // Esto es lo que define que usas Wallet
+    connectString: process.env.DB_CONNECT_STRING,
+    configDir: process.env.TNS_ADMIN,
+    walletLocation: process.env.TNS_ADMIN,
+    walletPassword: process.env.WALLET_PASSWORD
+    
+    // NOTA: No ponemos 'password' aquí. 
+    // El driver tomará el usuario de 'user' y la contraseña del Wallet.
+  });
 }
